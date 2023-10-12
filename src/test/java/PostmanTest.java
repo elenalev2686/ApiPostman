d73646c63e6802c07383393ca268df506fc57dea
elenalev2686 <elenalev2686@gmail.com>
2023-10-12 17:01:02 +0700

    success

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -13,7 +13,7 @@ public class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo(null))
+                .body("data", equalTo("some data"))
         ;
     }
 
