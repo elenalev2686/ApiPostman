fb1094b9beaa5004d221d525432c6398a1deac2a
elenalev2686 <elenalev2686@gmail.com>
2023-10-12 16:52:56 +0700

    initial commit

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+    @Test
+    void shouldPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+}
